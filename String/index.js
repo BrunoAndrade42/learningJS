a26d53c06aa1c80b00bda6427c68c575adcce786
NinjaAzul <erickdefg@gmail.com>
2022-06-15 20:44:30 -0300

    learning string js

---
+++ String/index.js
@@ -0,0 +1,81 @@
+
+// ====================================
+
+// //String alguns methodos de array 
+// const name = 'Erick de Freitas';
+
+// console.log(name[0])
+// // E
+
+
+// ======================================
+
+
+//split - divide a string em um array
+// const name = 'Erick de Freitas';
+
+// const arrName = name.split(' ');
+
+// // saida do array [ 'Erick', 'de', 'Freitas' ]
+
+// const first = arrName[0];
+
+// const last = arrName[2];
+
+// const avatar = `${first[0]} ${last[0]}`;
+
+// console.log(arrName);
+
+// console.log(avatar);
+
+// // ==========================================
+
+// let name = 'Erick de Freitas';
+
+// console.log(name.length);
+
+// // 16
+
+// // =======================================
+
+// // ==========================================
+
+// let name = 'https://github.com/NinjaAzul/JS/blob/main/Strings/perguntas.html';
+
+// console.log(name.indexOf('fastes'));
+
+// // =======================================
+
+// // ==========================================
+
+// let name = 'Erick de Freitass';
+
+// console.log(name.length > 20 ? name.substring(0, 10) + '...' : name);
+
+// // =======================================
+
+// // ==========================================
+
+// let name = 'bruno dos santos rocha';
+
+// //capitalize 
+// const capitalize = (string) => {
+//     const arr = string.split(' ');
+    
+//    return arr.map(word => {
+//         return word[0].toUpperCase() + word.substring(1);
+        
+//     }).join(' ');
+// }
+
+// console.log(capitalize(name));
+
+// // =======================================
+
+// // ==========================================
+
+// let name = '2022*06*15'.replace(/[*]/g, '/');
+
+// console.log(name);
+// // =======================================
+
