9b23711fc7edcbe9ce41a6c1093a9dd001834261
NinjaAzul <erickdefg@gmail.com>
2022-06-17 18:58:47 -0300

    months and days

--- Date/index.js
+++ Date/index.js
@@ -1,10 +1,82 @@
 
 // =======================================================
 
-const data = new Date()
+// const data = new Date()
 
-console.log(data)
+// console.log(data)
+// //2022-06-17T21:28:12.142Z
+// console.log(data.getFullYear())
+// //2022
+// console.log(data.getMonth())
+// //5
+// console.log(data.getDate())
+// //31
+// console.log(data.getHours())
+// //00
+// console.log(data.getMinutes())
+// //00
+// console.log(data.getSeconds())
+// //00
+// console.log(data.getMilliseconds())
+// //142
+// console.log(data.getTime())
 
-//2022-06-17T21:28:12.142Z 
+// console.log(data.getDay())
+
+// console.log(data.getUTCDate())
+
+// console.log(data.getUTCDay())
+
+// console.log(data.getUTCFullYear())
+
+// console.log(data.getUTCHours())
+
+// console.log(data.getUTCMinutes())
+
+// console.log(data.getUTCSeconds())
+
+
+// var birthday = new Date("December 17, 1995 03:24:00");
+// var birthday = new Date("1995-12-17T03:24:00");
+// var birthday = new Date(1995,11,17);
+// var birthday = new Date(1995,11,17,3,24,0);
+// const data = new Date("2022-05-31")
 
 // =======================================================
+
+
+// const data1 = new Date()
+// data1.setMonth(data1.getMonth() - 2)
+// data1.setFullYear(data1.getFullYear() - 1)
+// const month = data1.getMonth()
+
+// console.log(data1)
+// const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+// console.log(months[month])
+
+// =======================================================
+
+
+//   const days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+
+//    const data1 = new Date()
+
+// //    const day = data1.setDay(data1.getDay() - 1)
+
+// //     console.log(days[data1.getDay()])
+
+
+// console.log(new Date().getDay(), 'teste')
+
+//     console.log(days[new Date().getDay() - 1])
+
+// =======================================================
+
+const dataActual = new Date()
+
+const dataPrevious = new Date().getDate() - 1
+
+if(){
+
+}
